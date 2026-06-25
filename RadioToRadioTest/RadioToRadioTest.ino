aba922d83401647a3e96aa9dbf6fc06f3c00a4ee
PaskKat <katherine.pask@gmail.com>
2026-06-25 13:32:27 -0700

    updating workflow

--- RadioToRadioTest/RadioToRadioTest.ino
+++ RadioToRadioTest/RadioToRadioTest.ino
@@ -1,18 +1,39 @@
 // Only runs on ESP32S3 Dev Module board for debug port access.
+// onboard ESP32 sends message via wifi to another onboard esp32 every five seconds.
 
 #include <WiFi.h>
 #include <esp_now.h>
 #include <esp_wifi.h>
+
 #define WAIT_TIME 5000
 #define SECOND_TIME 1000
 
-uint8_t broadcastAddress[] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+
+uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 uint8_t sinkMAC[6];
 bool sinkMACKnown = false;
+
+// typedefs ---------------------------------------------------------------
+enum messageType : uint8_t {
+  DISCOVERY = 1,
+  JOIN_REQUEST,
+  TDMA_SCHEDULE,
+  SENSOR_DATA,
+  AGGREGATE_DATA
+};
+
+struct sensorDataPacket_t {
+  uint8_t type;
+  float temperature;
+  float humidity;
+  uint16_t soilMoisture;
+  unsigned long timestamp;
+};
 
 // MAIN --------------------------------------------------------------
 int lastTime = 0;
 int currentTime = 0;
+int messageCount = 0;
 void setup(){
   Serial.begin(9600);
   while(!Serial){;}
@@ -26,6 +47,15 @@ void setup(){
   }
 
   Serial.println("ESP32 radio serial ready");
+
+  // construct generic data.
+  sensorDataPacket_t myData = {
+    .type = SENSOR_DATA,
+    .temperature = 10,
+    .humidity = 20,
+    .temperature = 30,
+    .timestamp = 0
+  };
 }
 
 void loop() {
@@ -33,10 +63,12 @@ void loop() {
   while (currentTime - lastTime < WAIT_TIME){
     currentTime = millis();
   }
+
+  // execute sending task. broadcast for ease of reception.
+  // send ptr to message.
+  esp_now_send(broadcastAddress, &myData, sizeof(myData));
+
   // once they are equal, set to equal. then restart loop iteration.
   lastTime = currentTime;
-
-  // execute sending task.
-  esp_now_send();
 
 }
